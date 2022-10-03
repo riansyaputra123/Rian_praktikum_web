edb2f8e34573042b4d4b83f31baf77ac673a142e
Rian Syaputra Ainun Naim <92961223+riansyaputra123@users.noreply.github.com>
2022-10-03 21:52:09 +0800

    Add files via upload

---
+++ posttest3/javascript.js
@@ -0,0 +1,9 @@
+const warna= document.getElementById("warna");
+warna.style.color= '#00FFD1';
+// DOM dan addEventListener
+const putar_film = document.getElementById('cari');
+putar_film.addEventListener("click",
+    function(){
+        alert("Data Base Film Masih Kosong!😭😭" );
+    },
+)
